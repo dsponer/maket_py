0f01dca62f83af236037f9c44771a9ed7cb175dd
Pavel Efimov <dsponer157@gmail.com>
2020-02-20 21:52:53 +1000

    Add class

---
+++ main_without_pi.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+
+class Maket:
+    def __init__(self):
+        print('start class')
+
+    def military_base(self, conrtol_pin):
+        print('change red')
+        print('open roof')
+        print('start timer')
+        print('switch music to game')
+
+    def electrostation(self, control_pin):
+        print('change red')
+        print('switch off smoke')
+        print('switch music to dingg')
+
+    def goverment(self, conrtol_pin):
+        print('change red')
+        print('switch off light')
+        print('play song')
+
+    def weather_station(self, control_pin):
+        print('change red')
+
+    def rls_system(self, control_pin):
+        print('change red')
+
+    def home_build(self, control_pin):
+        print('change red')
+
+
+if __name__ == "__main__":
+    maket = Maket()
+    print('start work')
