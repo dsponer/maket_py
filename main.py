89a6b22afd559ca9680d757316acd51810fcc546
Pavel Efimov <dsponer157@gmail.com>
2020-02-25 13:06:42 +1000

    add work with socket

--- main.py
+++ main.py
@@ -1,16 +1,95 @@
-import RPi.GPIO as maket_pin
+import RPi.GPIO as maket_pins
 import time
+import multiprocessing as mp
+import socket
+import struct
 
-maket_pin.setmode(maket_pin.BOARD)
 
-maket_pin.setup(16, maket_pin.OUT)
-maket_pin.setup(18, maket_pin.OUT)
+def military_base(conrtol_pin):
+    print('change red')
+    print('open roof')
+    print('start timer')
+    print('switch music to game')
+    maket_pins.output(conrtol_pin, 1)
+    time.sleep(1)
+    maket_pins.output(conrtol_pin, 0)
+    time.sleep(1)
 
-maket_pin.output(16, 1)
-maket_pin.output(18, 1)
-time.sleep(10)
-maket_pin.output(16, 0)
-maket_pin.output(18, 0)
-time.sleep(1)
-maket_pin.cleanup()
 
+
+class Maket_CTF:
+    def __init__(self):
+        maket_pins.setmode(maket_pins.BOARD)
+        self.relay_list = (18, 16, 22, 24, 26, 40, 38, 36)
+        for pin in self.relay_list:
+            maket_pins.setup(pin, maket_pins.OUT)
+        print('start class')
+        self.host_name = '10.50.16.5'
+        self.port_name = 8080
+
+    def init_building(self):
+        for pin in self.relay_list:
+            maket_pins.output(pin, 0)
+
+    def test_code(self):
+        for pin in self.relay_list:
+            maket_pins.output(pin, 1)
+            time.sleep(1)
+        for pin in self.relay_list:
+            maket_pins.output(pin, 0)
+            time.sleep(1)
+
+    def electrostation(self, control_pin):
+        print('change red')
+        print('switch off smoke')
+        print('switch music to dingg')
+
+    def goverment(self, conrtol_pin):
+        print('change red')
+        print('switch off light')
+        print('play song')
+
+    def weather_station(self, control_pin):
+        print('change red')
+
+    def rls_system(self, control_pin):
+        print('change red')
+
+    def home_build(self, control_pin):
+        print('change red')
+
+    def clean_all(self):
+        maket_pins.cleanup()
+
+    def read_socket(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
+            self.s.connect((self.host_name, self.port_name))
+            while True:
+                data = self.s.recv(1024)
+                value = data.decode('utf-8')
+                value = int(value)
+                if value % 3 == 0:
+                    military_base(self.relay_list[0])
+
+                else:
+                    print(value)
+
+
+if __name__ == "__main__":
+    maket = Maket_CTF()
+    print('start work')
+    maket.init_building()
+
+    # proc = mp.Process(target=maket.read_socket, args=(message,))
+    # proc.start()
+    maket.read_socket()
+
+    # maket.test_code()
+    maket.clean_all()
+
+# maket_pin.output(16, 1)
+# maket_pin.output(18, 1)
+# time.sleep(10)
+# maket_pin.output(16, 0)
+# maket_pin.output(18, 0)
+# time.sleep(1)
--- server_test.py
+++ server_test.py
@@ -1,73 +1,18 @@
-from socket import *
-import selectors
-from datetime import datetime
-# import cv2
+import socket
+import time
 
-addr = ('10.50.16.5', 8000)
-selector = selectors.DefaultSelector()
-connections = []
-jpeg_quality = 60
+HOST = '10.50.16.5'  # Standard loopback interface address (localhost)
+PORT = 8080  # Port to listen on (non-privileged ports are > 1023)
 
-
-# class VideoGrabber:
-#     def __init__(self):
-#         self.encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), jpeg_quality]
-#         self.cap = cv2.VideoCapture(0)
-#         self.cap.set(cv2.CAP_PROP_FPS, 60)
-#         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-#         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-#         self.buffer = None
-#
-#     def get_bimage(self):
-#         success, img = self.cap.read()
-#         result, buffer = cv2.imencode('.jpg', img, self.encode_param)
-#         return buffer
-
-
-# grab = VideoGrabber()
-
-
-def server():
-    server_socket = socket(AF_INET, SOCK_DGRAM)
-    server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-    server_socket.bind(addr)
-
-    selector.register(server_socket, selectors.EVENT_READ, data)
-
-
-def data(client_socket):
-    request, addr = client_socket.recvfrom(4096)
-    request = bytes.decode(request)
-
-    if request:
-        # if request == 'get':
-        #     # buffer = grab.get_bimage()
-        #     if buffer is None:
-        #         return
-        #     if len(buffer) > 65507:
-        #         print(
-        #             "The message is too large to be sent within a single UDP datagram. We do not handle splitting the message in multiple datagrams")
-        #         client_socket.sendto("FAIL", addr)
-        #         return
-        #     # We sent back the buffer to the client
-        #     client_socket.sendto(buffer.tobytes(), addr)
-        # else:
-        #     print(request.split(','))
-        print(request)
-    else:
-        selector.unregister(client_socket)
-        client_socket.close()
-
-
-def event_loop():
-    while True:
-        events = selector.select()  # (key,events)
-
-        for key, _ in events:
-            callback = key.data
-            callback(key.fileobj)
-
-
-if __name__ == '__main__':
-    server()
-    event_loop()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        count = 0
+        while True:
+            conn.sendall(str(count).encode())
+            print(count)
+            time.sleep(1)
+            count += 1
